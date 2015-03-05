8bb7feb053cadacc44e8606233c15b302a2136e2
Ben Southgate <bsouthga@gmail.com>
2015-03-05 15:15:46 -0500

    first commit

--- embed.src.js
+++ embed.src.js
@@ -13,9 +13,18 @@
 ;(function() {
 
 // Select this script
-var script = document.querySelector(
+var scripts = document.querySelectorAll(
   'script[src="http://datatools.urban.org/features/embed.js"]'
 );
+
+// increment global count of urban embeded
+var count = this._URBAN_EMBED_COUNT = (
+  this._URBAN_EMBED_COUNT === undefined ? 0 :
+  this._URBAN_EMBED_COUNT + 1
+);
+
+// get this script tag
+var script = scripts[count];
 
 // get requested data viz
 var viz = script.getAttribute('data-viz');
@@ -30,7 +39,7 @@ urban_frame.id = "urban_frame_" + viz;
 script.parentNode.insertBefore(urban_frame, script);
 
 // add pymParent to global object
-this.pymParent = new pym.Parent(
+this['pymParent_' + urban_frame.id] = new pym.Parent(
   urban_frame.id,
   "http://datatools.urban.org/features/" + viz,
   {}
